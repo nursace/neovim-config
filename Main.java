a1f36b466a1f522e712fb2223e7e127dac9ff2d4
nursace <n.bolotbek@wb-digital.ru>
2024-03-27 16:24:33 +0600

    added nvim-tmux-navigator

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util
 
 public class Main {
   static long last = 42;
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,14 @@ vim.cmd('set tabstop=2')
 vim.cmd('set softtabstop=2')
 vim.cmd('set shiftwidth=2')
 vim.g.mapleader = ' '
+vim.g.background = 'light'
+
+-- navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.wo.number=true
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
